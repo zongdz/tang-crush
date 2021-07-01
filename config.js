00dbbaecaa0a469261d70805dfe5d8377fb55e12
zongdz <86786954+zongdz@users.noreply.github.com>
2021-07-01 21:13:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'bé bầu iu zấu à!<3<3',
+    introTitle: 'bé iu à!<3<3',
     introDesc: `Trái đất vốn lạ thường
     Mà sao bé cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -9,10 +9,10 @@ const CONFIG = {
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bé lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu anh vọng nhiều nhiều <33333333',
-    mess: 'anh biết mà 🥰. iu bé bầu nhiều nhiều 😘😘',
+    mess: 'anh biết mà 🥰. iu bé nhiều nhiều 😘😘',
     messDesc: 'buổi tối vui vẻ nha bé iu.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
